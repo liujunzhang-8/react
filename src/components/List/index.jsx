6f61f157c4281446961245d36b5c202c7e2d1405
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-04-22 12:39:06 +0800

    note

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -4,20 +4,23 @@
  * @version:
  * @Date: 2023-04-22 09:36:36
  * @LastEditors: Gorgio.Liu
- * @LastEditTime: 2023-04-22 10:14:29
+ * @LastEditTime: 2023-04-22 11:41:07
  */
 import React, { Component } from "react";
 import Item from "../Item";
 import './index.css'
 
-
 export default class List extends Component {
   render() {
+    const {todos} = this.props
     return (
       <ul className="todo-main">
-        <Item />
-        <Item />
-        <Item />
+        {
+          todos.map(todo => {
+            // return <Item key={todo.id} id={todo.id} name={todo.name} done={todo.done} />
+            return <Item key={todo.id} {...todo} />
+          })
+        }
       </ul>
     );
   }
