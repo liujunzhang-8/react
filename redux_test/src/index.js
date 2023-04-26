8ba69b7e17c972da82edcc838b6eb266f7edb5b1
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-04-27 00:01:50 +0800

    redux

--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -4,16 +4,22 @@
  * @version: 
  * @Date: 2023-04-26 21:12:24
  * @LastEditors: Gorgio.Liu
- * @LastEditTime: 2023-04-26 21:15:17
+ * @LastEditTime: 2023-04-26 22:49:48
  */
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
+// import store from "./redux/store";
+
+// store.subscribe(() => {
+//   // 渲染app组件到页面
+//   ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+// })
 
 // 渲染app组件到页面
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
---
+++ redux_test/src/redux/count_action.js
@@ -0,0 +1,28 @@
+/*
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-04-26 22:52:20
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-04-26 23:54:04
+ */
+/**
+ * 该文件专门为Count组件生成action对象
+ */
+
+import {INCREMENT, DECREMENT} from './constant'
+
+// 同步action，就是指action的值为Object类型的一般对象
+export const createIncrementAction = data => ({type: INCREMENT, data})
+export const createDecrementAction = data => ({type: DECREMENT, data})
+
+// 异步action，就是指action的值为函数，异步action中一般都会调用同步action
+export const createIncrementAsyncAction = (data, time) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(createIncrementAction(data))
+    }, time)
+  }
+}
+
+
