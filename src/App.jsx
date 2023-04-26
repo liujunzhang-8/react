1334f3b8fd3ece6c1ea1af646dd323e7a1c51f6c
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-04-26 17:23:45 +0800

    note

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,28 @@
-import React, { Component } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home"; // Home 是路由组件
-import About from "./pages/About"; // About 是路由组件
-import Header from "./components/Header";  // Header是一般组件
-import MyNavLink from "./components/MyNavLink";
+import React, { Component } from 'react'
+import { Button, DatePicker } from 'antd';
+import {
+  WechatOutlined,
+  WeiboOutlined,
+  SearchOutlined
+} from '@ant-design/icons';
 
 export default class App extends Component {
   render() {
+    const onChange = (date, dateString) => {
+      console.log(date, dateString);
+    };
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-8">
-            <Header />
-          </div>
-        </div>
-          <div className="row">
-            <div className="col-xs-offset-2 col-2">
-              <div className="list-group">
-                {/* 原生html中，靠<a>跳转不同的页面 */}
-                {/* <a href="./about" className="list-group-item">About</a>
-                <a href="" className="list-group-item">Home</a> */}
-                {/* 在React中靠路由链接实现切换组件 --- 编写路由链接 */}
-                {/* <NavLink activeclassname='active' className="list-group-item" to="/about">
-                  About
-                </NavLink>
-                <NavLink activeclassname='active' className="list-group-item" to="/home">
-                  Home
-                </NavLink> */}
-                <MyNavLink to="/about">About</MyNavLink>
-                <MyNavLink to="/home">Home</MyNavLink>
-              </div>
-            </div>
-            <div className="col-6">
-              <div className="panel">
-                <div className="panel-body">
-                  {/* 注册路由 */}
-                    <Routes>
-                      <Route path="/about" element={<About />} />
-                      <Route path="/home/*" element={<Home />} />
-                      <Route path="*" element={<Navigate to="/home" replace  />} />
-                    </Routes>
-                </div>
-              </div>
-            </div>
-          </div>
+        <Button type="primary">按钮1</Button>
+        <Button type="link">按钮2</Button>
+        <WechatOutlined />
+        <WeiboOutlined />
+        <Button type="primary" icon={<SearchOutlined />}>
+          Search
+        </Button>
+        <DatePicker onChange={onChange} />
       </div>
-    );
+    )
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@
  * @version: 
  * @Date: 2023-04-21 21:52:50
  * @LastEditors: Gorgio.Liu
- * @LastEditTime: 2023-04-24 22:36:26
+ * @LastEditTime: 2023-04-26 16:12:45
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import App from './App'
 
 // 渲染app组件到页面
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
