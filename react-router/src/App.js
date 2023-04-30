c26c0cb7f771df5072e67ffea54e904a042333bc
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-04-30 10:28:05 +0800

    note

---
+++ react-router/src/App.js
@@ -0,0 +1,54 @@
+/*
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-04-29 20:08:04
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-04-29 20:57:02
+ */
+import React from 'react'
+import { NavLink, useRoutes } from "react-router-dom";
+import routes from './routes';
+
+
+export default function App() {
+  // 根据路由表生成对应的路由规则
+  const element = useRoutes(routes)
+  return (
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-8">
+          <div className="page-header">
+            <h2>React Router Demo</h2>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-2">
+          <div className="list-group">
+            <NavLink className="list-group-item" to="/about">
+              About
+            </NavLink>
+            <NavLink className="list-group-item" to="/home">
+              Home
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="panel">
+            <div className="panel-body">
+              {/* 注册路由 */}
+              {/* <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Navigate to="/about" />} />
+              </Routes> */}
+              {element}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
---
+++ react-router/src/pages/Message.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-04-30 10:23:12
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-04-30 10:23:19
+ */
+import React from 'react'
+
+export default function Message() {
+  return (
+    <div>Message组件</div>
+  )
+}
