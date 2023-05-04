25c4f130996a99e169c83c467a19ec0b4fe92795
Gorgio Liu <junchang.liu@meditrusthealth.cn>
2023-05-04 15:39:25 +0800

    zhufeng-react

---
+++ zhufeng-react/react_01/01_src_react简易版实现/constants.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 
+ * @Author: Gorgio.Liu
+ * @version: 
+ * @Date: 2023-05-04 14:44:20
+ * @LastEditors: Gorgio.Liu
+ * @LastEditTime: 2023-05-04 14:45:01
+ */
+
+// 表示这是一个文本类型的元素 在源码里面没有这样一个类型
+export const REACT_TEXT = Symbol('REACT_TEXT')
--- zhufeng-react/react_01/src/index.js
+++ zhufeng-react/react_01/src/index.js
@@ -4,27 +4,21 @@
  * @version: 
  * @Date: 2023-05-02 22:18:14
  * @LastEditors: Gorgio.Liu
- * @LastEditTime: 2023-05-02 22:52:43
+ * @LastEditTime: 2023-05-04 15:37:36
  */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from './react';
+import ReactDOM from './react-dom';
 
 /**
- * 老得版本中我们只要用到CSS，就需要在顶部引入react变量，但是在新的版本中，不需要再引入React变量了。
- * JSX：javascript + xml html
- * JSX其实只是react提供的一个语法糖
- * React元素是构建React应用的最小单位，也就是所谓的虚拟DOM
- * 虚拟DOM
- * 1. JSX表达式 表达式就是变量 常量 操作符 混合在一起的组合
- * 表达式是可以计算的，而且 肯定会有一个返回值
- * jsx更像JS
+ * 函数组件其实就是一个函数，接收props，返回一个React元素
  */
+function FunComp(props) {
+  // return <h1>Hello, {props.name}</h1>
+  return React.createElement('h1', null, 'Hello ', props.name);
+}
 
-let element = <h1>Hello React</h1>
-// jsx 在执行的时候其实是一个函数调用，它是一个创建元素的工厂函数
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {element}
-  </React.StrictMode>
-);
+// let element = <Welcome name='React' />
+let element = React.createElement(FunComp, {name: 'React'});
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'));
